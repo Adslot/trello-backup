f1f43187dffbf985b47918d219d3ed4558463a8c
Tim Coombs <git@slowb.ro>
2016-09-09 11:45:28 +1000

    Update to Python3, Argparse, and Fixing requests
    - Fix: use requests `params` instead of `data`
    - Feature: Backup all boards under Org, instead of boards user is on
    - Feature: Argparse instead of configparser
    - Feature: Output by logger, instead of print

--- README.md
+++ README.md
@@ -5,6 +5,28 @@ This simple app backs up your Trello boards as JSON files to a local directory.
 
 Setup
 -----
-- Make a copy of OR rename the `trello-backup.config.sample` file to `trello-backup.config`
-- Edit the `trello-backup.config` file to add your Trello API Key and Token
-- Run the `trello-backup.py` script
+- run `trello-backup.py -a "AppName" -d directory_to_save_json` and follow the prompts to Generate a Trello API Key and OAuth Token
+
+
+```
+usage: trello-backup.py [-h] [-k KEY] [-t OAUTH_TOKEN] -d DIRECTORY [-l LOG]
+                        -a APPNAME [-o ORGID] [-n ORGNAME]
+
+Trello Backup
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -k KEY, --key KEY     Trello API Key
+  -t OAUTH_TOKEN, --oauth-token OAUTH_TOKEN
+                        Trello OAuth Token
+  -d DIRECTORY, --directory DIRECTORY
+                        Directory to save JSON files, created if non exist
+  -l LOG, --log LOG     Logging level - info|INFO|WARNING|ERROR|CRITICAL
+  -a APPNAME, --appname APPNAME
+                        Trello App Name
+  -o ORGID, --orgid ORGID
+                        Organisation by ID to backup.
+  -n ORGNAME, --orgname ORGNAME
+                        Organisation by Name to backup
+
+```
--- trello-backup.py
+++ trello-backup.py
@@ -1,127 +1,153 @@
 #!/usr/bin/env python -u
-
-import ConfigParser
-import os, sys
+from __future__ import unicode_literals
+import os
+import sys
 import json
 import requests
 import time
 import io
+import logging
+import argparse
 
-configFile = 'trello-backup.config'
-configFile = os.path.join(os.path.abspath(os.path.dirname(__file__)), configFile)
-
-API_KEY = TOKEN = API_URL = ''
-
-def main():
-	if os.path.isfile(configFile):
-		config = ConfigParser.RawConfigParser()
-		config.read(configFile)
-	else:
-		sys.exit('Config file "{0}" does not exist.'.format(configFile))
-
-	global API_KEY, TOKEN, API_URL
-	API_KEY = config.get('Credentials', 'API_KEY')
-	TOKEN = config.get('Credentials', 'TOKEN')
-
-	API_URL = config.get('Paths', 'API_URL')
-	OUTPUT_DIRECTORY = config.get('Paths', 'OUTPUT_DIRECTORY')
-
-	TOKEN_EXPIRATION = config.get('Options', 'TOKEN_EXPIRATION')
-	APP_NAME = config.get('Options', 'APP_NAME')
-	ORGANIZATION_IDS = config.get('Options', 'ORGANIZATION_IDS')
-	ORGANIZATION_NAMES = config.get('Options', 'ORGANIZATION_NAMES')
-	PRETTY_PRINT = config.get('Options', 'PRETTY_PRINT') == 'yes'
-
-	if not API_KEY:
-		print('You need an API key to run this app.')
-		print('Visit this url: https://trello.com/1/appKey/generate')
-		API_KEY = raw_input('Then enter the API key here: ')
-		print('\n[IMPORTANT] Make sure to add the key to the config file.\n')
-		raw_input('Press enter to continue...\n')
-
-	if not TOKEN:
-		print('You need a token to run this app.')
-		print("Visit this url: {0}connect?key={1}&name={2}&response_type=token&expiration={3}".format(API_URL, API_KEY, APP_NAME, TOKEN_EXPIRATION))
-		TOKEN = raw_input('Then enter the token here: ')
-		print('\n[IMPORTANT] Make sure to add the token to the config file.\n')
-		raw_input('Press enter to continue...\n')
-
-	if ORGANIZATION_NAMES and not ORGANIZATION_IDS:
-		ORGANIZATION_IDS = get_organization_ids(ORGANIZATION_NAMES)
-
-	# Parameters to get list of boards
-	boardsPayload = {
-		'key':API_KEY,
-		'token':TOKEN,
-		'filter':'open',
-		'lists':'open',
-	}
-	# Parameters to get board contents
-	boardPayload = {
-		'key':API_KEY,
-		'token':TOKEN,
-		'lists':'open',
-		'fields':'all',
-		'actions':'all',
-		'action_fields':'all',
-		'actions_limit':'1000',
-		'cards':'all',
-		'card_fields':'all',
-		'card_attachments':'true',
-		'lists':'all',
-		'list_fields':'all',
-		'members':'all',
-		'member_fields':'all',
-		'checklists':'all',
-		'checklist_fields':'all',
-		'organization':'false',
-	}
-	boards = requests.get(API_URL + "members/my/boards", data=boardsPayload)
-	try:
-		if len(boards.json()) <= 0:
-			print('No boards found.')
-			return
-	except ValueError:
-		print('Unable to access your boards. Check your key and token.')
-		return
-	if not os.path.exists(OUTPUT_DIRECTORY):
-		os.makedirs(OUTPUT_DIRECTORY)
-
-	print('Backing up boards:')
-	epoch_time = str(int(time.time()))
-
-	for board in boards.json():
-		if ORGANIZATION_IDS and (not board["idOrganization"] or not board["idOrganization"] in ORGANIZATION_IDS):
-			continue
-
-		print(u"    - {0} ({1})".format(board["name"], board["id"]))
-		boardContents = requests.get(API_URL + "boards/" + board["id"], data=boardPayload)
-		with io.open(OUTPUT_DIRECTORY + u'/{0}_'.format(board["name"].replace("/","-")) + epoch_time + '.json', 'w', encoding='utf8') as file:
-			args = dict( sort_keys=True, indent=4) if PRETTY_PRINT else dict()
-			data = json.dumps(boardContents.json(), ensure_ascii=False, **args)
-			file.write(unicode(data))
-
-def get_organization_ids(ORGANIZATION_NAMES):
-	selected_organizations = []
-	for organization in ORGANIZATION_NAMES.split(','):
-		selected_organizations.append(organization.strip())
-	organization_ids = []
-
-	# Parameters to get a list of organizations
-	organizationsPayload = {
-		'key':API_KEY,
-		'token':TOKEN,
-	}
-
-	organizations = requests.get(API_URL + "members/my/organizations", data=organizationsPayload)
-	if len(organizations.json()) <= 0:
-		print('No organizations found.')
-	else:
-		for organization in organizations.json():
-			if organization["name"] in selected_organizations:
-				organization_ids.append(organization["id"])
-	return organization_ids
+API_URL = "https://api.trello.com/1/"
 
 
-if __name__ == '__main__':
-	main()
+def get_organization_ids(ORGANIZATION_NAMES, API_KEY, TOKEN):
+    """
+    Translates Org Name to ID
+    """
+    selected_organizations = []
+    for organization in ORGANIZATION_NAMES.split(','):
+        selected_organizations.append(organization.strip())
+    organization_ids = []
+
+    # Parameters to get a list of organizations
+    organizationsPayload = {
+        'key': '{}'.format(API_KEY),
+        'token': '{}'.format(TOKEN),
+    }
+
+    organizations = requests.get(API_URL + "members/me/organizations", params=organizationsPayload)
+    if len(organizations.json()) <= 0:
+        logger.error('No organizations found.')
+    else:
+        logger.info('Finding all Organizations.')
+        for organization in organizations.json():
+            if organization["name"].lower() in selected_organizations:
+                organization_ids.append(organization["id"])
+                logger.debug('Org Added: {}'.format(organization["id"]))
+    return organization_ids
+
+if __name__ == "__main__":
+
+    # parse args
+    parser = argparse.ArgumentParser(description="Trello Backup")
+    parser.add_argument("-k", "--key", help="Trello API Key")
+    parser.add_argument("-t", "--oauth-token", help="Trello OAuth Token")
+    parser.add_argument("-d", "--directory", required=True, help="Directory to save JSON files, created if non exist")
+    parser.add_argument("-l", "--log", help="Logging level - info|INFO|WARNING|ERROR|CRITICAL")
+    parser.add_argument("-a", "--appname", required=True, help="Trello App Name")
+    parser.add_argument("-o", "--orgid", help="Organisation by ID to backup. ")
+    parser.add_argument("-n", "--orgname", help="Organisation by Name to backup")
+
+    args = parser.parse_args()
+
+    API_KEY = args.key
+    TOKEN = args.oauth_token
+    OUTPUT_DIRECTORY = args.directory
+    APP_NAME = args.appname
+    ORGANIZATION_IDS = ""
+    ORGANIZATION_NAMES = ""
+    if args.orgid:
+        ORGANIZATION_IDS = args.orgid.lower()
+    if args.orgname:
+        ORGANIZATION_NAMES = args.orgname.lower()
+    PRETTY_PRINT = 'yes'
+
+    if args.log is not None:
+        log_level = args.log.upper()
+    else:
+        log_level = "INFO"
+    # Create our logger
+    logger = logging.getLogger('root')
+    logger.setLevel(log_level)
+    ch = logging.StreamHandler()
+    ch.setLevel(log_level)
+    formatter = logging.Formatter('%(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    if not API_KEY:
+        logger.error('You need an API key to run this app.')
+        logger.error('Visit this url: https://trello.com/1/appKey/generate, and re-run script')
+        sys.exit(1)
+
+    if not TOKEN:
+        logger.error('You need a token to run this app.')
+        logger.error("Visit this url: {0}connect?key={1}&name={2}&response_type=token&expiration={3}".format(
+            API_URL,
+            API_KEY,
+            APP_NAME,
+            "never"))
+        logger.error("Then re-run script")
+        sys.exit(1)
+
+    if ORGANIZATION_NAMES and not ORGANIZATION_IDS:
+        ORGANIZATION_IDS = get_organization_ids(ORGANIZATION_NAMES, API_KEY, TOKEN)
+        if len(ORGANIZATION_IDS) < 1:
+            logger.error('Finding Org ID failed.')
+            sys.exit(1)
+
+    # Parameters to get list of boards
+    boardsPayload = {
+        'key': '{}'.format(API_KEY),
+        'token': '{}'.format(TOKEN),
+        'filter': 'open',
+        'lists': 'open',
+    }
+    # Parameters to get board contents
+    boardPayload = {
+        'key': '{}'.format(API_KEY),
+        'token': '{}'.format(TOKEN),
+        'lists': 'open',
+        'fields': 'all',
+        'actions': 'all',
+        'action_fields': 'all',
+        'actions_limit': '1000',
+        'cards': 'all',
+        'card_fields': 'all',
+        'card_attachments': 'true',
+        'lists': 'all',
+        'list_fields': 'all',
+        'members': 'all',
+        'member_fields': 'all',
+        'checklists': 'all',
+        'checklist_fields': 'all',
+        'organization': 'false',
+    }
+
+    for org_id in ORGANIZATION_IDS:
+        boards = requests.get(API_URL + "organizations/" + org_id + "/boards", params=boardsPayload)
+        try:
+            if len(boards.json()) <= 0:
+                logger.info('No boards found under Organisation ID: {}'.format(org_id))
+        except ValueError:
+            logger.error('Unable to access your boards. Check your key and token.')
+            sys.exit(1)
+        if not os.path.exists(OUTPUT_DIRECTORY):
+            os.makedirs(OUTPUT_DIRECTORY)
+
+        logger.info('Backing up boards:')
+        epoch_time = str(int(time.time()))
+
+        for board in boards.json():
+            if ORGANIZATION_IDS and (not board["idOrganization"] or not board["idOrganization"] in ORGANIZATION_IDS):
+                continue
+
+            logger.info(" {0} ({1})".format(board["name"], board["id"]))
+            boardContents = requests.get(API_URL + "boards/" + board["id"], params=boardPayload)
+            with io.open(OUTPUT_DIRECTORY + '/{0}_'.format(board["name"].replace("/", "-")) + epoch_time + '.json',
+                         'w', encoding='utf8') as file:
+                args = dict(sort_keys=True, indent=4) if PRETTY_PRINT else dict()
+                data = json.dumps(boardContents.json(), ensure_ascii=False, **args)
+                file.write(data)
